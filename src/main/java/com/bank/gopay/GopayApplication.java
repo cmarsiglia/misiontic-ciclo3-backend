0e611e11b61c2d24ab4f1650a532c1c93dc3cc7c
Carlos Marsiglia <carlos.marsiglia@gmail.com>
2022-12-20 09:06:16 -0500

    Initial commit

---
+++ src/main/java/com/bank/gopay/GopayApplication.java
@@ -0,0 +1,13 @@
+package com.bank.gopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GopayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GopayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/gopay/GopayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.gopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
